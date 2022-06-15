8c7d5ffda3dd46edf4fb67c409c370ad98b864d0
Brianna Baldwin <105931108+Brianna-Baldwin@users.noreply.github.com>
2022-06-15 15:28:26 -0600

    Start on games page

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-games',
+  selector: 'games',
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.scss'],
 })
@@ -9,22 +9,26 @@ export class GamesComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-
   games = [
     {
       id: 1,
-      staticLink: 'https://lh4.googleusercontent.com/-gZiu96oTuu4/Uag5oWLQHfI/AAAAAAAABSE/pl1W8n91hH0/w140-h165-no/Homer-Static.png',
-      activeLink: 'https://lh4.googleusercontent.com/i1RprwcvxhbN2TAMunNxS4RiNVT0DvlD9FNQCvPFuJ0=w140-h165-no'
+      img: 'https://i.pinimg.com/originals/5f/3b/74/5f3b74e3a8a9341222b12781433b0602.jpg',
     },
     {
       id: 2,
-      staticLink: 'https://lh4.googleusercontent.com/-gZiu96oTuu4/Uag5oWLQHfI/AAAAAAAABSE/pl1W8n91hH0/w140-h165-no/Homer-Static.png',
-      activeLink: 'https://lh4.googleusercontent.com/i1RprwcvxhbN2TAMunNxS4RiNVT0DvlD9FNQCvPFuJ0=w140-h165-no'
+      img: 'https://media.vandal.net/i/1200x630/12-2019/2019122011265139_1.jpg',
     },
     {
-      id: 2,
-      staticLink: 'https://lh4.googleusercontent.com/-gZiu96oTuu4/Uag5oWLQHfI/AAAAAAAABSE/pl1W8n91hH0/w140-h165-no/Homer-Static.png',
-      activeLink: 'https://lh4.googleusercontent.com/i1RprwcvxhbN2TAMunNxS4RiNVT0DvlD9FNQCvPFuJ0=w140-h165-no'
+      id: 3,
+      img: 'https://wallpaperaccess.com/full/1288165.jpg',
     },
+    {
+      id: 4,
+      img: 'https://i.pinimg.com/originals/c6/2e/a6/c62ea6568c7c5bfbcdc38a094b9f6c87.jpg'
+    },
+    {
+      id: 5,
+      img: 'https://3.bp.blogspot.com/-qn916QiwfFo/XR40yv-QXoI/AAAAAAAAIGc/tXmSlUxtyzUqItbJFa0JqTQT3hblFTuEgCKgBGAs/w919/elden-ring-logo-uhdpaper.com-4K-2.jpg'
+    }
   ];
 }
