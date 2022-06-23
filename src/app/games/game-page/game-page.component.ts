b18c0e7c7a4cf7a91b6ad14cfee239cb320ecdfd
Brianna Baldwin <105931108+Brianna-Baldwin@users.noreply.github.com>
2022-06-23 11:19:40 -0600

    Added show more/less button to videos pages

--- src/app/games/game-page/game-page.component.ts
+++ src/app/games/game-page/game-page.component.ts
@@ -16,62 +16,42 @@ export class GamePageComponent implements OnInit {
   gameId!: number;
   game!: IGame;
   videos!: IVideo[];
-  backgroundImg!: string;
-  end!: number;
 
-  showMore = true; // find a way to get this to work
-
+  end = 3;
+  showMore = true;
 
   constructor(private route: ActivatedRoute, private gamesService: GamesService, private el: ElementRef) { }
 
   ngOnInit(): void {
+    // get the game and video Ids to display all the videos for the correct game
     this.routeSub = this.route.params.subscribe(params => {
       this.gameId = params['id'];
       this.game = this.gamesService.getGame(this.gameId - 1);
       this.videos = this.gamesService.getVideos(this.gameId - 1);
-
-      this.backgroundImg = this.game.img;
-      console.log(this.backgroundImg)
-
-      let style: string = `
-      .bg-img {
-        background-image: url('${this.backgroundImg}');
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-attachment: fixed;
-        background-position: bottom;
-      }
-     `;
-
-     this.createStyle(style);
     });
   }
 
-  createStyle(style: string): void {
-    const styleElement = document.createElement('style');
-    styleElement.appendChild(document.createTextNode(style));
-    this.el.nativeElement.appendChild(styleElement);
-  }
-
-  checkLength() {
-    if(this.showMore === false) {
+  // method checks to see if showMore is true or false and displays amount of videos based on variable
+  checkShowMore() {
+    if(this.showMore == false) {
       this.end = 3;
+      this.showMore = true;
     }
     else {
       this.end = 21;
+      this.showMore = false;
     }
-  // only show first three items unless showMore = true
-  // if showMore = true then show all items and hide button
   }
 
-  get sortData() {
+  // sorts videos by date
+  get sortVideos() {
     return this.videos.sort((a, b) => {
       return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
     });
   }
 
   ngOnDestroy() {
-    this.routeSub.unsubscribe();
+    this.routeSub.unsubscribe(); // unsubscribe to route if switching to a different game
   }
 
 }
